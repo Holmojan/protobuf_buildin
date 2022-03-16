5d1e84fbef46ce120e6a0a48819db5636c60e8ef
Holmojan <holmojan@live.cn>
2022-03-16 22:11:44 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿
+#define PB_BUILDIN__USE_JSON_SERIALIZER
+#define PB_BUILDIN__USE_BINARY_SERIALIZER
+#include "pb_buildin/pb_buildin.hpp"
+
+
+int main()
+{
+	pb_buildin::Test::A a;
+	a.set_x(123);
+	auto s = a.SerializeAsString();
+
+	return 0;
+}
