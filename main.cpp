b83f094a751d1138832be6c91d24b97b2b3c6ae9
Holmojan <holmojan@live.cn>
2022-09-23 10:48:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,19 @@
 
 namespace pb_buildin {
 
-#include "declare.hpp"
+	PB_PACKAGE(Test)
+
+		PB_MESSAGE(A)
+			PB_OPTIONAL(sint32, x, 1)
+			PB_OPTIONAL(bool, y, 2)
+			PB_OPTIONAL(string, z, 3)
+
+			PB_OPTIONAL(bytes, t, 4)
+
+			PB_MAP(int32, string, m, 1024)
+		PB_MESSAGE_END
+
+	PB_PACKAGE_END
 
 }
 
