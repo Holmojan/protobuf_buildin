1fa7ba8d7a73af15f87251605f172cfd1148e656
Holmojan <holmojan@live.cn>
2024-01-04 13:28:26 +0800

    Add files via upload

---
+++ json_value.patch.cpp
@@ -0,0 +1,44 @@
+
+bool Value::isConvertibleTo(ValueType other) const {
+  switch (other) {
+  case nullValue:
+    return (isNumeric() && asDouble() == 0.0) ||
+           (type_ == booleanValue && value_.bool_ == false) ||
+           (type_ == stringValue && asString() == "") ||
+           (type_ == arrayValue && value_.map_->size() == 0) ||
+           (type_ == objectValue && value_.map_->size() == 0) ||
+           type_ == nullValue;
+#if defined(JSON_HAS_INT64)
+  case intValue:
+      return isInt64() ||
+          (type_ == realValue && InRange(value_.real_, minInt64, maxInt64)) ||
+          type_ == booleanValue || type_ == nullValue;
+  case uintValue:
+      return isUInt64() ||
+          (type_ == realValue && InRange(value_.real_, 0, maxUInt64)) ||
+          type_ == booleanValue || type_ == nullValue; 
+#else
+  case intValue:
+    return isInt() ||
+           (type_ == realValue && InRange(value_.real_, minInt, maxInt)) ||
+           type_ == booleanValue || type_ == nullValue;
+  case uintValue:
+    return isUInt() ||
+           (type_ == realValue && InRange(value_.real_, 0, maxUInt)) ||
+           type_ == booleanValue || type_ == nullValue;
+#endif
+  case realValue:
+    return isNumeric() || type_ == booleanValue || type_ == nullValue;
+  case booleanValue:
+    return isNumeric() || type_ == booleanValue || type_ == nullValue;
+  case stringValue:
+    return isNumeric() || type_ == booleanValue || type_ == stringValue ||
+           type_ == nullValue;
+  case arrayValue:
+    return type_ == arrayValue || type_ == nullValue;
+  case objectValue:
+    return type_ == objectValue || type_ == nullValue;
+  }
+  JSON_ASSERT_UNREACHABLE;
+  return false;
+}
